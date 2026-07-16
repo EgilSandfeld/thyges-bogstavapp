91dc80435d45ac26fadc1cdb07fc372931d58adc
Egil Sandfeld <sandfeld@gmail.com>
2026-07-16 14:39:27 +0200

    Refresh offline cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'thyges-bogstavapp-v1';
+const CACHE_NAME = 'thyges-bogstavapp-v2';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
