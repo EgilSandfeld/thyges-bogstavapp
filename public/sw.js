505b5abbc22756ca5220d7b2a943e21a91c06062
Egil Sandfeld <sandfeld@gmail.com>
2026-07-16 14:55:18 +0200

    Refresh offline cache for realistic textures

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'thyges-bogstavapp-v2';
+const CACHE_NAME = 'thyges-bogstavapp-v3';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
@@ -15,9 +15,9 @@ self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET')
     return;
 
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+  event.respondWith(fetch(event.request).then(response => {
     if (response && response.status === 200 && response.type !== 'opaque')
       caches.open(CACHE_NAME).then(cache => cache.put(event.request, response.clone()));
     return response;
-  }).catch(() => caches.match('./index.html'))));
+  }).catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html'))));
 });
